7f8ed0709e443579ca443eeaa0bbf459ae596611
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-07-24 09:31:30 -0400

    added book object

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+let myLibary = [];
+
+
+
+
+
+
+
+
+
+
+/**
+ *  Constructor to create a Book object. 
+ * 
+ * @param {*} title  Book title 
+ * @param {*} author Author of book 
+ * @param {*} pages  Number of pages in book 
+ * @param {*} read   If the user has read the book 
+ */
+function Book(title, author, pages, read)
+{
+    this.title = title; 
+    this.author = author; 
+    this.pages = pages; 
+    this.read = read; 
+}
